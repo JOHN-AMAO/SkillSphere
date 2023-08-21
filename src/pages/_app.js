f8bba96bdeee6ebd20cfc43846af305ea3c47849
JOHN-AMAO <amaojohn767@gmail.com>
2023-08-21 17:35:44 +0100

    "First commit"

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,10 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ClerkProvider {...pageProps}>
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
 }
