1f533ab3da2ca88ba7173f99f5373c91847fd38f
JOHN-AMAO <amaojohn767@gmail.com>
2023-08-22 03:44:41 +0100

    bebugging vc feature

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
       <nav class='bg-white border-gray-200 dark:bg-gray-900 w-full'>
         <div class='max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4'>
           <a
-            href='/VideoCall/lobby.html'
+            href='/videocall/lobby.html'
             class='flex items-center'
           >
             <span class='self-center text-2xl font-semibold whitespace-nowrap dark:text-white'>
