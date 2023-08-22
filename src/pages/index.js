9ecd634d3efa61ab043683c32303a86bac1038c2
JOHN-AMAO <amaojohn767@gmail.com>
2023-08-22 04:04:10 +0100

    "Authentication routes"

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 "use client";
 import TypewriterComponent from "typewriter-effect";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -80,12 +81,20 @@ export default function Home() {
                 </a>
               </li>
               <li>
-                <a
-                  href='#'
-                  class='block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'
+                <Link
+                  href='/sign-in'
+                  class='bg-green-400 block py-1 pl-5 pr-5 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent  md:hover:text-blue-700  dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'
                 >
-                  Contact
-                </a>
+                  Sign In
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href='/sign-up'
+                  class='bg-green-700 block py-1 pl-5 pr-5 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent  md:hover:text-blue-700  dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'
+                >
+                  Sign Up
+                </Link>
               </li>
             </ul>
           </div>
