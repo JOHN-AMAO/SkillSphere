2d9ccfe322be19db99f8478b179bd0dce21d3974
JOHN-AMAO <amaojohn767@gmail.com>
2023-08-22 05:04:34 +0100

    "Paystack Intergration"

--- src/pages/index.js
+++ src/pages/index.js
@@ -73,17 +73,17 @@ export default function Home() {
                 </a>
               </li>
               <li>
-                <a
-                  href='#'
-                  class='block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'
+                <Link
+                  href='/payments'
+                  class='bg-green-700 block py-1 pl-5 pr-5 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent  md:hover:text-blue-700  dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'
                 >
-                  Pricing
-                </a>
+                  Payments
+                </Link>
               </li>
               <li>
                 <Link
                   href='/sign-in'
-                  class='bg-green-400 block py-1 pl-5 pr-5 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent  md:hover:text-blue-700  dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'
+                  class='bg-green-500 block py-1 pl-5 pr-5 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent  md:hover:text-blue-700  dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'
                 >
                   Sign In
                 </Link>
@@ -91,7 +91,7 @@ export default function Home() {
               <li>
                 <Link
                   href='/sign-up'
-                  class='bg-green-700 block py-1 pl-5 pr-5 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent  md:hover:text-blue-700  dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'
+                  class='bg-green-400 block py-1 pl-5 pr-5 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent  md:hover:text-blue-700  dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'
                 >
                   Sign Up
                 </Link>
---
+++ src/pages/payments/index.js
@@ -0,0 +1,119 @@
+import React, { useEffect } from "react";
+import Script from "next/script";
+
+const Index = () => {
+  useEffect(() => {
+    const paymentForm = document.getElementById("paymentForm");
+
+    function payWithPaystack(e) {
+      e.preventDefault();
+
+      const handler = PaystackPop.setup({
+        key: "pk_test_4b44a2eab590b32da2919b0d5b98d4283b51e21e", // Replace with your public key
+        email: document.getElementById("email-address").value,
+        amount: document.getElementById("amount").value * 100,
+        ref: "" + Math.floor(Math.random() * 1000000000 + 1),
+        onClose: function () {
+          alert("Window closed.");
+        },
+        callback: function (response) {
+          const message = "Payment complete! Reference: " + response.reference;
+          alert(message);
+        },
+      });
+
+      handler.openIframe();
+    }
+
+    if (paymentForm) {
+      paymentForm.addEventListener("submit", payWithPaystack, false);
+    }
+  }, []);
+
+  const handleScriptLoad = () => {
+    // Script loaded, you can now use it
+    console.log("Paystack script loaded");
+  };
+
+  return (
+    <div className='w-full  bg-slate-900'>
+      <div className='flex items-center justify-center bg-slate-900'>
+        <div className='bg-black p-4 rounded shadow'>
+          <div className='bg-gray-800 p-8 rounded-lg shadow-lg w-96'>
+            <form id='paymentForm'>
+              <div className='mb-4'>
+                <label
+                  htmlFor='email'
+                  className='text-white font-semibold mb-1 block'
+                >
+                  Email Address
+                </label>
+                <input
+                  type='email'
+                  id='email-address'
+                  required
+                  className='w-full p-2 bg-gray-700 rounded text-white'
+                />
+              </div>
+              <div className='mb-4'>
+                <label
+                  htmlFor='amount'
+                  className='text-white font-semibold mb-1 block'
+                >
+                  Amount
+                </label>
+                <input
+                  type='tel'
+                  id='amount'
+                  required
+                  className='w-full p-2 bg-gray-700 rounded text-white'
+                />
+              </div>
+              <div className='mb-4'>
+                <label
+                  htmlFor='first-name'
+                  className='text-white font-semibold mb-1 block'
+                >
+                  First Name
+                </label>
+                <input
+                  type='text'
+                  id='first-name'
+                  className='w-full p-2 bg-gray-700 rounded text-white'
+                />
+              </div>
+              <div className='mb-4'>
+                <label
+                  htmlFor='last-name'
+                  className='text-white font-semibold mb-1 block'
+                >
+                  Last Name
+                </label>
+                <input
+                  type='text'
+                  id='last-name'
+                  className='w-full p-2 bg-gray-700 rounded text-white'
+                />
+              </div>
+              <div className='text-center'>
+                <button
+                  type='submit'
+                  className='bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded'
+                >
+                  Pay
+                </button>
+              </div>
+            </form>
+          </div>
+
+          <Script
+            src='https://js.paystack.co/v1/inline.js'
+            onLoad={handleScriptLoad}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Index;
