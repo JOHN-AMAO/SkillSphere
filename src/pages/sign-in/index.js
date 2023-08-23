a520f377a033e4aca10c435c04a067911afcc5c1
JOHN-AMAO <amaojohn767@gmail.com>
2023-08-23 11:12:19 +0100

    "styling"

--- src/pages/sign-in/index.js
+++ src/pages/sign-in/index.js
@@ -1,5 +1,10 @@
+import React from "react";
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignIn />;
+  return (
+    <div className='min-h-screen flex items-center justify-center'>
+      <SignIn />
+    </div>
+  );
 }
